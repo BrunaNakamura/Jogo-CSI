0a4c2b0a921da6be94a5f2235f478dd909072c4c
BrunaNakamura <73372594+BrunaNakamura@users.noreply.github.com>
2020-11-01 19:39:36 -0300

    Update JOGO CSI.cpp

--- JOGO CSI.cpp
+++ JOGO CSI.cpp
@@ -5,51 +5,46 @@ main ()
 	int sim=0;
 	do
 	{
-		printf ("Telefonou para a vitima? (1-Sim/2-Nao)");
+		printf ("Telefonou para a vitima? (1-Sim/Nao)");
 		scanf("%d",&p1);
 		if (p1==1)
 		{
 			sim++;
 		}
-		printf("Esteve no local do crime? (1-Sim/2-Nao)");
+		printf("Esteve no local do crime? (1-Sim/Nao)");
 		scanf("%d",&p2);
 		if (p2==1)
 		{
 			sim++;
 		}
-		printf("Mora perto da vitima? (1-Sim/2-Nao)");
+		printf("Mora perto da vitima? (1-Sim/Nao)");
 		scanf("%d",&p3);
 		if (p3==1)
 		{
 			sim++;
 		}
-		printf("Devia para a vitima? (1-Sim/2-Nao)");
+		printf("Devia para a vitima? (1-Sim/Nao)");
 		scanf("%d",&p4);
 		if (p4==1)
 		{
 			sim++;
 		}
-		printf("Ja estudou com a vitima? (1-Sim/2-Nao)");
+		printf("Ja estudou com a vitima? (1-Sim/Nao)");
 		scanf("%d",&p5);
 		if (p5==1)
 		{
 			sim++;
 		}
-			else if (sim==2)
+			switch (sim)
 			{
-				printf("SUSPEITA.\n");
-			}
-			else if (sim==3||sim==4)
-			{
-				printf("CUMPLICE.\n");
-			}
-			else if (sim==5)
-			{
-				printf("ASSASSINO.\n");
-			}
-			else if (sim==1||sim==0)
-			{
-				printf("INOCENTE.\n");
+				case 0 ... 1: printf("INOCENTE.\n");
+					break;
+				case 2: printf("SUSPEITA.\n");
+					break;
+				case 3 ... 4:printf("CUMPLICE.\n");
+					break;
+				case 5: printf("ASSASSINO.\n");
+					break;
 			}
 		printf("Deseja continuar? (1-Sim/2-Nao)");
 		scanf("%d",&cont);
